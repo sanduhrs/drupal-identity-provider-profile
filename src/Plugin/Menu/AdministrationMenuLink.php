b69078e90f50975ffc8cc8cd20d5162174ffa88d
sanduhrs <stefan@auditor.email>
2022-10-10 07:02:29 +0200

    Programmatically and conditionally add admin menu links

---
+++ src/Plugin/Menu/AdministrationMenuLink.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\identity_provider\Plugin\Menu;
+
+use Drupal\Core\Menu\MenuLinkDefault;
+
+/**
+ * Menu link class.
+ */
+class AdministrationMenuLink extends MenuLinkDefault {}
